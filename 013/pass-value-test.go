e3a7406e7734a1c295ea3f80a57dc1fcbb4205c8
Kapeel Mopkar <kapeel_mopkar@persistent.com>
2018-12-24 16:12:21 +0530

    Files updated

--- 013/pass-value-test.go
+++ 013/pass-value-test.go
@@ -5,13 +5,19 @@ import (
 )
 
 func main() {
+
 	x := 0
-	foo(x)
-	fmt.Println(x)
+	fmt.Println("x before", &x)
+	fmt.Println("x before", x)
+	foo(&x)
+	fmt.Println("x after", &x)
+	fmt.Println("x after", x)
 }
 
-func foo(y int) {
-	fmt.Println(y)
-	y = 43
-	fmt.Println(y)
+func foo(y *int) {
+	fmt.Println("y before", y)
+	fmt.Println("y before", *y)
+	*y = 43
+	fmt.Println("y after", y)
+	fmt.Println("y after", *y)
 }
--- 014/sort-test-2.go
+++ 014/sort-test-2.go
@@ -12,6 +12,8 @@ type person struct {
 
 type byAge []person
 
+type byName []person
+
 func (a byAge) Len() int {
 	return len(a)
 }
@@ -22,6 +24,18 @@ func (a byAge) Swap(i, j int) {
 
 func (a byAge) Less(i, j int) bool {
 	return a[i].age < a[j].age
+}
+
+func (a byName) Len() int {
+	return len(a)
+}
+
+func (a byName) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func (a byName) Less(i, j int) bool {
+	return a[i].name < a[j].name
 }
 
 func (p person) String() string {
@@ -38,5 +52,8 @@ func main() {
 
 	fmt.Println(people)
 	sort.Sort(byAge(people))
+	fmt.Println(people)
+
+	sort.Sort(byName(people))
 	fmt.Println(people)
 }
